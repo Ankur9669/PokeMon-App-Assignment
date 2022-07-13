950f343234850eec8f2a1f65765007a36583bb61
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-07-13 19:39:26 +0530

    update

--- pokemon-app/src/pages/auth/login/Login.jsx
+++ pokemon-app/src/pages/auth/login/Login.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { useScript } from "../../../hooks/useScript";
 import {
   decodeJWTToken,
   useAppDispatch,
@@ -10,6 +11,7 @@ import "./login.css";
 const Login = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const googleButtonRef = useRef();
   const CLIENT_ID =
     "81019992991-rdkss3s6rqnbrv9psadoosl05epn4q7g.apps.googleusercontent.com";
 
@@ -24,20 +26,38 @@ const Login = () => {
     navigate("/pokemonlisting");
   };
 
-  useEffect(() => {
-    const google = window.google;
-    google.accounts.id.initialize({
+  useScript("https://accounts.google.com/gsi/client", () => {
+    window.google.accounts.id.initialize({
       client_id: CLIENT_ID,
       callback: handleCallBackResponse,
     });
 
-    // Rendering the sign in button
-    google.accounts.id.renderButton(document.getElementById("sign-in-div"), {
+    //  Rendering the sign in button
+    window.google.accounts.id.renderButton(googleButtonRef.current, {
       theme: "outline",
       size: "large",
     });
-  }, []);
-  return <div id="sign-in-div" className="login-page"></div>;
+  });
+
+  return (
+    <div id="sign-in-div" className="login-page">
+      <div ref={googleButtonRef}></div>
+    </div>
+  );
 };
 
 export default Login;
+
+// useEffect(() => {
+//   const google = window.google;
+//   google.accounts.id.initialize({
+//     client_id: CLIENT_ID,
+//     callback: handleCallBackResponse,
+//   });
+
+//   // Rendering the sign in button
+//   google.accounts.id.renderButton(document.getElementById("sign-in-div"), {
+//     theme: "outline",
+//     size: "large",
+//   });
+// }, []);
